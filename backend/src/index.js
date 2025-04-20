9a7cbb43e40032f178a9165d692aa5850d9203bd
hriteshransingh <hriteshransing67220@gmail.com>
2025-04-20 14:50:35 +0530

    Fix:smaill change in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,15 +17,16 @@ app.use(
   })
 );
 
+
+
 app.use("/api", aiRoutes);
 
-if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res)=> {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+
 
 
 
